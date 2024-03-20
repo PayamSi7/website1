751d27717a69a50a80d77895e52b3cb697d33ffc
Payam79 <160176346+Payam79@users.noreply.github.com>
2024-03-21 01:16:26 +0430

    comment

--- order/views.py
+++ order/views.py
@@ -55,42 +55,42 @@ CallbackURL = 'http://127.0.0.1:8080/verify/'
 
 def send_request(request):
     data = {
-        "MerchantID": settings.MERCHANT,
+    #    "MerchantID": settings.MERCHANT,
         "Amount": amount,
         "Description": description,
         "Phone": phone,
         "CallbackURL": CallbackURL,
     }
-    data = json.dumps(data)
+    #data = json.dumps(data)
     # set content length by data
     headers = {'content-type': 'application/json', 'content-length': str(len(data)) }
-    try:
-        response = requests.post(ZP_API_REQUEST, data=data,headers=headers, timeout=10)
+    #try:
+    #    response = requests.post(ZP_API_REQUEST, data=data,headers=headers, timeout=10)
 
-        if response.status_code == 200:
-            response = response.json()
-            if response['Status'] == 100:
-                return {'status': True, 'url': ZP_API_STARTPAY + str(response['Authority']), 'authority': response['Authority']}
-            else:
-                return {'status': False, 'code': str(response['Status'])}
-        return response
+    #    if response.status_code == 200:
+    #        response = response.json()
+    #        if response['Status'] == 100:
+    #            return {'status': True, 'url': ZP_API_STARTPAY + str(response['Authority']), 'authority': response['Authority']}
+    #        else:
+    #            return {'status': False, 'code': str(response['Status'])}
+    #    return response
     
-    except requests.exceptions.Timeout:
-        return {'status': False, 'code': 'timeout'}
-    except requests.exceptions.ConnectionError:
-        return {'status': False, 'code': 'connection error'}
+    #except requests.exceptions.Timeout:
+    #    return {'status': False, 'code': 'timeout'}
+    #except requests.exceptions.ConnectionError:
+    #    return {'status': False, 'code': 'connection error'}
 
 
 def verify(authority):
     data = {
-        "MerchantID": settings.MERCHANT,
+    #    "MerchantID": settings.MERCHANT,
         "Amount": amount,
         "Authority": authority,
     }
-    data = json.dumps(data)
+    #data = json.dumps(data)
     # set content length by data
     headers = {'content-type': 'application/json', 'content-length': str(len(data)) }
-    response = requests.post(ZP_API_VERIFY, data=data,headers=headers)
+    #response = requests.post(ZP_API_VERIFY, data=data,headers=headers)
 
     if response.status_code == 200:
         response = response.json()
